e5aa12492e11499dae6b15653ddff5596f82abc0
Robson Ribeiro <113537578+robrib1820@users.noreply.github.com>
2025-01-31 21:41:09 -0300

    activity

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,13 +1,22 @@
 const input = document.querySelector("#favchap");
-const button = document.querySelector("button");
 const list = document.querySelector("#list");
+const button = document.querySelector("button")
 
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
+button.addEventListener("click", function(){
+    if (input.value.trim() !== "") {
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
 
-li.textContent = input.value;
-deleteButton.textContent = "❌";
+        li.append(deleteButton);
 
-li.append(deleteButton);
-
-list.append(li);
+        list.append(li);
+        input.value = "";
+        input.focus();
+        
+        deleteButton.addEventListener("click", function() {
+            list.removeChild(li);
+        })
+    }
+})
