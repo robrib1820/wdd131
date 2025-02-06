2f0e725cc292d79e7f864074a5d06efa5671a831
Robson Ribeiro <113537578+robrib1820@users.noreply.github.com>
2025-02-05 21:55:00 -0300

    temples change

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -5,8 +5,10 @@ document.getElementById("lastModified").innerText = `© Robson Ribeiro - all rig
 const mainNav = document.querySelector(".navigation");
 const hamButton = document.querySelector("#menu");
 
-
-hamButton.addEventListener("click", () => {
+const nombre = () => {
     mainNav.classList.toggle("show");
-    hamButton.classList.toggle("show");
-})
+    hamButton.classList.toggle("show");}
+
+
+
+hamButton.addEventListener("click", nombre)
