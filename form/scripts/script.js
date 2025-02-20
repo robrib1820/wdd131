b89f996b0dd55c0fe028e950d16b6eb83d65c683
Robson Ribeiro <113537578+robrib1820@users.noreply.github.com>
2025-02-20 18:54:00 -0300

    form actitivy

---
+++ form/scripts/script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+    ];
+
+    const productSelect = document.getElementById("product");
+    products.forEach(product => {
+        const option = document.createElement("option");
+        option.value = product.id;
+        option.textContent = product.name;
+        productSelect.appendChild(option);
+    });
+});
+
+if (window.location.pathname.includes("review.html")) {
+    document.addEventListener("DOMContentLoaded", function () {
+        let reviewCount = localStorage.getItem("reviewCount") || 0;
+        reviewCount++;
+        localStorage.setItem("reviewCount", reviewCount);
+        document.body.innerHTML += `<p>Written Review: ${reviewCount}</p>`;
+    });
+}
